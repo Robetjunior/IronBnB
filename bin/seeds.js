09eb252dd9982b90e056c6df291151184bcb6447
Jose Roberto <juniorferreirarob@gmail.com>
2020-07-11 17:25:45 -0300

    created seeds

---
+++ bin/seeds.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Host = require('../models/Host.model');
+
+require('../configs/db.config');
+
+const host = [
+    {local: 'Bela Vista', espaco: 'Quarto inteiro', qntHosp: 3},
+    {local: 'Bela Vista', espaco: 'Espaco inteiro', qntHosp: 2},
+    {local: 'Bela Vista', espaco: 'Quarto compartilhado', qntHosp: 1},
+    {local: 'Vila Mariana', espaco: 'Quarto inteiro', qntHosp: 5},
+    {local: 'Vila Mariana', espaco: 'Espaco inteiro', qntHosp: 2},
+    {local: 'Vila Mariana', espaco: 'Quarto compartilhado', qntHosp: 3},
+    {local: 'Jardins', espaco: 'Quarto inteiro', qntHosp: 10},
+    {local: 'Jardins', espaco: 'Espaco inteiro', qntHosp: 4},
+    {local: 'Jardins', espaco: 'Quarto compartilhado', qntHosp: 2},
+    {local: 'Campinas', espaco: 'Quarto inteiro', qntHosp: 7},
+    {local: 'Campinas', espaco: 'Espaco inteiro', qntHosp: 15},
+    {local: 'Campinas', espaco: 'Quarto compartilhado', qntHosp: 3},
+    {local: 'Jardins', espaco: 'Quarto inteiro', qntHosp: 2},
+    {local: 'Jardins', espaco: 'Espaco inteiro', qntHosp: 20},
+    {local: 'Jardins', espaco: 'Quarto compartilhado', qntHosp: 5},
+];
+
+(async function seedDB(){
+    try{
+        const result = await Host.create(host);
+        console.log(`Success! Created ${result.length} hosts added to db`);
+        mongoose.connection.close();
+    }catch(err){
+        console.error(err);
+    }
+})()
