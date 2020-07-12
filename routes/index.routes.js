802d31f52f9971d222f755b8a9d8493cfcc92898
Jose Roberto <juniorferreirarob@gmail.com>
2020-07-12 13:47:47 -0300

    add routes to find all hosts

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,6 +5,16 @@ const Host = require('../models/Host.model')
 /* GET home page */
 router.get('/', (req, res) => res.render('index'));
 
-
+//filtra todos os hosts
+router.get('/search', async (req, res) =>{
+    try{
+        const localData = await Host.find();
+        console.log({localData})
+        // return res.send({localData})
+        res.render('search', {host: localData})
+    }catch(err){
+        throw new Error(err)
+    }
+});
 
 module.exports = router;
