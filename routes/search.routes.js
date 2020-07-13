ebdc273c082c8694e189d41b1e73b55091b57dd2
Jose Roberto <juniorferreirarob@gmail.com>
2020-07-13 11:04:26 -0300

    initial front details-host & changes search.routes.js

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -9,7 +9,6 @@ const mongoose = require('mongoose');
 router.get('/search', async (req, res) =>{
     try{
         const localData = await Host.find({"local":req.query.search});
-        console.log({localData})
         // return res.send({localData})
         res.render('search', {host: localData})
     }catch(err){
@@ -22,11 +21,20 @@ router.get('/search/:hostId', async (req, res) =>  {
     try{
         const hostId = await Host.findById(req.params.hostId);
         // res.send({hostId})
-        res.render('guest/details-host', {host : hostId})
+        res.render('guest/details-host', {host : hostId});
     }catch(err){
-        console.error(err);
+        throw new Error(err);
     }
 });
+
+router.get('/search/:hostId/reserva', async (req, res) => {
+    try{
+        const hostId = await Host.findById(req.params.hosId);
+        res.render('guest/confirm-host', {host: hostId});
+    }catch(err){
+        throw new Error(err);
+    }
+})
 
 
 module.exports = router;
