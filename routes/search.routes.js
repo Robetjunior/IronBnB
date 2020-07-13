752834e1ba269210db39b6538cd63b44efbd50c8
Jose Roberto <juniorferreirarob@gmail.com>
2020-07-13 10:45:46 -0300

    update search.routes.js

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -3,8 +3,30 @@ const router = new Router();
 const Host = require('../models/Host.model');
 const mongoose = require('mongoose');
 
-router.get('/search', (req, res) => res.render('search'));
+// router.get('/search', (req, res) => res.render('search'));
 
-// router.post('/')
+//filtra todos os hosts
+router.get('/search', async (req, res) =>{
+    try{
+        const localData = await Host.find({"local":req.query.search});
+        console.log({localData})
+        // return res.send({localData})
+        res.render('search', {host: localData})
+    }catch(err){
+        throw new Error(err)
+    }
+});
+
+//Direciona e mostra o details do host selecionado
+router.get('/search/:hostId', async (req, res) =>  {
+    try{
+        const hostId = await Host.findById(req.params.hostId);
+        // res.send({hostId})
+        res.render('guest/details-host', {host : hostId})
+    }catch(err){
+        console.error(err);
+    }
+});
+
 
 module.exports = router;
