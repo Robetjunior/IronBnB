dde4510ef364447a4899e2eb7f8768d941419897
Jose Roberto <juniorferreirarob@gmail.com>
2020-07-14 11:58:19 -0300

    init Reserva.model

--- models/Reserva.model.js
+++ models/Reserva.model.js
@@ -2,7 +2,28 @@ const { Schema, model } = require('mongoose');
 
 const reservaSchema = new Schema (
     {
-        
+        startDate: {
+            type: Date,
+            required: true
+        },
+        endDate: {
+            type: Date,
+            required: true
+        },
+        guestId: {
+            type: String,
+            require: true,
+            trim: true
+        },
+        hostId: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        totalValue: {
+            type: Number,
+            required: true
+        }
     }
 )
 
