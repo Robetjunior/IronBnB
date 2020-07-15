eac5faadd02e68427bf5d6cef9723fbca37842a3
Eduardo <eduardo_muller0301@Hotmail.com>
2020-07-15 04:30:40 -0300

    search.css

--- public/javascripts/searchDetailsArrow.js
+++ public/javascripts/searchDetailsArrow.js
@@ -1,35 +1,40 @@
-let expander = $('.expander');
 let expanderArrow = $('.expander-arrow');
 let info = $('#info');
 let description = $('.description');
-let expanded = false;
-
-  expander.on( 'click', function() {
-  expand();
-})
+let expanded = false; 
 
 function expand() {
   if( expanded ) {
     info.css('height', '100px' );
-    expander.css('bottom', '90px');
+    
     description.css('display', 'none');
-    expanderArrow.removeClass('ion-chevron-down');
-    expanderArrow.addClass('ion-chevron-up');
-  } else {
-    info.css('height', '200px' );
-    expander.css('bottom', '190px');
-    description.css('display', 'block');
     expanderArrow.removeClass('ion-chevron-up');
     expanderArrow.addClass('ion-chevron-down');
+  } else {
+    info.css('height', '200px' );
+    
+    description.css('display', 'block');
+    expanderArrow.removeClass('ion-chevron-down');
+    expanderArrow.addClass('ion-chevron-up');
   }
   expanded = !expanded;
 }
 
 let expList = document.getElementsByClassName("expander");
 let expAdd = 0;
-for (let i = 0; i < expList.length; i++) {
+
+for (let i = 0; i < expList.length; i++) { 
   expAdd++;
   expList[i].id = "expansor" + expAdd;
+  let expander = $('.expander');
+  expander.on( 'click', function() {
+    if (expanded){
+      expander.css('bottom', '90px');
+    } else {
+    expander.css('bottom', '190px');
+    }
+    expand();
+  })
 }
 
 
@@ -37,15 +42,14 @@ let classes = document.getElementsByClassName("description");
 let idnum = 0;
 for (let i = 0; i < classes.length; i++) {
   idnum++;
-  classes[i].id = "descricao" + idnum;
+  classes[i].id = "descript" + idnum;
 
 }
 
 
-// let arrows = document.getElementsByClassName("expander-arrow");
-// let idnum = 0;
-// for (let i = 0; i < arrows.length; i++) {
-//   idMore++;
-//   arrows[i].id = "expander-arrow" + idMore;
-// }
-
+let arrows = document.getElementsByClassName("expander-arrow");
+let idMore= 0;
+for (let i = 0; i < arrows.length; i++) {
+  idMore++;
+  arrows[i].id = "expander-arrow" + idMore;
+}
