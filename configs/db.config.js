391b104a680acb626c7f55f2faadc5d58f09f287
Jose Roberto <juniorferreirarob@gmail.com>
2020-07-17 14:53:39 -0300

    update db.config

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI || process.env.MONGOLAB_PUCE_URI;
 
 mongoose
   .connect(MONGODB_URI, {
