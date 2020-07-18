e481271b4756a98cd7a0266cd6b81b6ad65c083a
Eduardo <eduardo_muller0301@Hotmail.com>
2020-07-17 23:25:10 -0300

    cards ok" 1

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');   
 const MONGODB_URI = process.env.MONGODB_URI || process.env.MONGOLAB_PUCE_URI;
 console.log(MONGODB_URI)
+
 mongoose
   .connect(MONGODB_URI, {
     useNewUrlParser: true,
@@ -12,3 +13,4 @@ mongoose
     console.error(`An error ocurred trying to connect to the database ${MONGODB_URI}: `, error);
     process.exit(1);
   });
+  
